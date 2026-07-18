35112385c28e1d6e624da7ddb3b55625b936ae0a
mtnihrbp-hue <mtnihrbp@gmail.com>
2026-07-18 16:30:24 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
-from datetime import datetime
-print("Gold Premium Monitor started")
-print(datetime.utcnow())
+import requests
+
+url = "https://api.gold-api.com/price/XAU"
+
+try:
+    r = requests.get(url, timeout=15)
+    r.raise_for_status()
+
+    data = r.json()
+
+    print("=" * 40)
+    print("Gold API Connected")
+    print("=" * 40)
+    print(data)
+
+except Exception as e:
+    print(e)
