af41e98861927f504486865b993f198ef446d6ed
mtnihrbp-hue <mtnihrbp@gmail.com>
2026-07-19 16:25:15 +0330

    Update daric.py

--- src/collector/daric.py
+++ src/collector/daric.py
@@ -4,12 +4,19 @@ URL = "https://apisc.daric.gold/loan/api/v1/User/Collateral/GetGoldlPrice"
 
 
 def get_daric_price():
-    response = requests.get(URL, timeout=30)
+    response = requests.get(
+        URL,
+        timeout=30,
+        headers={
+            "User-Agent": "Mozilla/5.0"
+        }
+    )
+
     response.raise_for_status()
 
     data = response.json()
 
     return {
         "platform": "Daric",
-        "priceIRR": float(data["Data"]["BestSellPrice"]) * 10
+        "price": float(data["Data"]["BestSellPrice"]) * 10
     }
