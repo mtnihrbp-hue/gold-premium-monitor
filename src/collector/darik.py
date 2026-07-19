4abe1d68278f10b0371e6e7e6455c090b351d043
mtnihrbp-hue <mtnihrbp@gmail.com>
2026-07-19 15:14:53 +0330

    Create darik.py

---
+++ src/collector/darik.py
@@ -0,0 +1,15 @@
+import requests
+
+URL = "https://apisc.daric.gold/loan/api/v1/User/Collateral/GetGoldlPrice"
+
+
+def get_daric_price():
+    response = requests.get(URL, timeout=15)
+    response.raise_for_status()
+
+    data = response.json()
+
+    return {
+        "platform": "Daric",
+        "price": float(data["Data"]["BestSellPrice"])
+    }
