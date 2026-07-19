87a3c6caba2f4dc8d609e980aef6373ab43de8e6
mtnihrbp-hue <mtnihrbp@gmail.com>
2026-07-19 15:22:06 +0330

    Create wallgold.py

---
+++ src/collector/wallgold.py
@@ -0,0 +1,15 @@
+import requests
+
+URL = "https://api.wallgold.ir/api/v1/price?side=buy&symbol=GLD_18C_750TMN"
+
+
+def get_wallgold_price():
+    response = requests.get(URL, timeout=15)
+    response.raise_for_status()
+
+    data = response.json()
+
+    return {
+        "platform": "WallGold",
+        "price": float(data["result"]["price"])
+    }
