cf4ab72470e839a21b520b33eba5a8d768e58840
mtnihrbp-hue <mtnihrbp@gmail.com>
2026-07-19 14:39:19 +0330

    Update milli.py

--- src/collector/milli.py
+++ src/collector/milli.py
@@ -1,15 +1,15 @@
 import requests
 
-
 URL = "https://milli.gold/api/v1/public/milli-price/external"
 
 
 def get_milli_price():
+    response = requests.get(URL, timeout=15)
+    response.raise_for_status()
 
-    r = requests.get(URL, timeout=15)
+    data = response.json()
 
-    r.raise_for_status()
-
-    data = r.json()
-
-    return int(data["price"])
+    return {
+        "platform": "Milli",
+        "price": float(data["data"]["price18"]["date"])
+    }
