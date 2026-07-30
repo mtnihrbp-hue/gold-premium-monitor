72a0f8bb6b2b5a3c06b4711f9c0ab71a1b98fa9f
mtnihrbp-hue <mtnihrbp@gmail.com>
2026-07-30 14:06:19 +0330

    Update iran.py

--- src/collector/iran.py
+++ src/collector/iran.py
@@ -4,6 +4,7 @@ from collector.wallgold import get_wallgold_price
 from collector.taline import get_taline_price
 from collector.hoorgold import get_hoorgold_price
 from collector.parasteh import get_parasteh_price
+from collector.daric import get_daric_price
 
 
 def get_market_prices():
@@ -16,6 +17,7 @@ def get_market_prices():
         get_wallgold_price,
         get_hoorgold_price,
         get_parasteh_price,
+        get_daric_price,
         get_taline_price,
     ]
 
