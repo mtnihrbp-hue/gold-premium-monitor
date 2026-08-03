058978079cc40e6e134b9034a24163882272f8d0
mtnihrbp-hue <mtnihrbp@gmail.com>
2026-08-03 10:54:21 +0330

    Update wallgold.py

--- src/collector/wallgold.py
+++ src/collector/wallgold.py
@@ -4,7 +4,7 @@ URL = "https://api.wallgold.ir/api/v1/price?side=buy&symbol=GLD_18C_750TMN"
 
 
 def get_wallgold_price():
-    response = requests.get(URL, timeout=15)
+    response = requests.get(URL, timeout=10)
     response.raise_for_status()
 
     data = response.json()
