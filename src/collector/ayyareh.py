405a45a00adebb6419b1c8795ecb9717b15fa66d
mtnihrbp-hue <mtnihrbp@gmail.com>
2026-08-03 10:50:52 +0330

    Update ayyareh.py

--- src/collector/ayyareh.py
+++ src/collector/ayyareh.py
@@ -4,7 +4,7 @@ URL = "https://ayyareh.com/general/getGoldPrice.php"
 
 
 def get_ayyareh_price():
-    response = requests.get(URL, timeout=15)
+    response = requests.get(URL, timeout=10)
     response.raise_for_status()
 
     data = response.json()
